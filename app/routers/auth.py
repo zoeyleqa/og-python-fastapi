555399f4e589ac0f3db09a9ddf327f7a9dc3fa6c
Brandon Shea <bsshea5@gmail.com>
2021-09-30 11:19:38 -0700

    added CORS middleware with definition of accepted origins

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,25 +2,27 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from app import schemas
 from app.hashing import Hash
-from app import token 
+from app import token
 from app.models import User
 from sqlmodel import Session, select
 from app.database import get_session
 
-router = APIRouter(
-  tags=['Authentication']
-)
+router = APIRouter(tags=["Authentication"])
 
-@router.post('/login', response_model=schemas.Token)
-def login(*, session: Session = Depends(get_session), request: OAuth2PasswordRequestForm = Depends()):
-  user = session.exec(select(User).where(User.username == request.username)).one()
 
-  if not user or not Hash.verify(request.password, user.password):
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND, 
-      detail="Invalid Credentials"
-    )
+@router.post("/login", response_model=schemas.Token)
+def login(
+    *,
+    session: Session = Depends(get_session),
+    request: OAuth2PasswordRequestForm = Depends()
+):
+    user = session.exec(select(User).where(User.username == request.username)).one()
 
-  access_token = token.create_access_token(data={"sub": user.email})
+    if not user or not Hash.verify(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
+        )
 
-  return {"access_token": access_token, "token_type": "bearer"}
+    access_token = token.create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, users, attendees, roles, languages, languageCategories
 
 app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-  # create table if not already existing
-  # create_db_and_tables()
-  print("FastAPI Service started...")
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:1337",
+    "http://cop.oakgrovetech.com/",
+    "https://cop.oakgrovetech.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
@@ -15,3 +26,10 @@ app.include_router(attendees.router)
 app.include_router(roles.router)
 app.include_router(languages.router)
 app.include_router(languageCategories.router)
+
+
+@app.on_event("startup")
+def on_startup():
+    # create table if not already existing
+    # create_db_and_tables()
+    print("FastAPI Service started...")
