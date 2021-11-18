f08948803a8cfad7c9f69b57adad5c52df15fa64
zoeyleqa@gmail.com <18235400+zoeyleqa@users.noreply.github.com>
2021-11-18 03:24:13 -0500

    default user roeuting with fastapi crud

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,33 +1,43 @@
-from app.database import get_session
-from app.models import User
+from fastapi_crudrouter import SQLAlchemyCRUDRouter as CRUDRouter
 from fastapi import APIRouter, Depends, status, HTTPException
-from app import schemas, oauth2
+from app.database import get_session, get_db
+from app.models import User
+from app.schemas import UserRead, UserCreate
+from app import oauth2
 from app.services import users
 from sqlmodel import Session
 from app.hashing import Hash
 from typing import List
 
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"],
-    # lock it down!
-    # dependencies=[Depends(oauth2.get_current_user)],
-    responses={},
+# router = APIRouter(
+#     prefix="/users",
+#     tags=["Users"],
+#     # lock it down!
+#     # dependencies=[Depends(oauth2.get_current_user)],
+#     responses={},
+# )
+
+router = CRUDRouter(
+    schema=UserRead,
+    create_schema=UserCreate,
+    db=get_db,
+    db_model=User,
+    prefix="users"
 )
 
 
-@router.post("", response_model=schemas.UserRead, status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=UserRead, status_code=status.HTTP_201_CREATED)
 def create(
     *,
     session: Session = Depends(get_session),
     # current_user: User = Depends(oauth2.get_current_user),
-    args: schemas.UserCreate,
+    args: UserCreate,
 ):
     return users.create(args, session)
 
 
-@router.get("", response_model=List[schemas.UserRead], status_code=status.HTTP_200_OK)
-def show_all(*, session: Session = Depends(get_session)):
+@router.get("", response_model=List[UserRead], status_code=status.HTTP_200_OK)
+def get_all(*, session: Session = Depends(get_session)):
     all_users = users.show_all(session)
 
     if not all_users:
@@ -39,14 +49,3 @@ def show_all(*, session: Session = Depends(get_session)):
     return all_users
 
 
-@router.get("/{id}", response_model=schemas.UserRead, status_code=status.HTTP_200_OK)
-def show(*, session: Session = Depends(get_session), id: int):
-    user = users.show(id, session)
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with the id {id} is not available.",
-        )
-
-    return user
